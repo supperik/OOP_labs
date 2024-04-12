cbe105ebc648b8bc9655a565681bc3a7ee3879fd
supperik <mr.niki234@mail.ru>
2024-04-12 17:41:37 +0300

    52

--- lab1/bin2dec/cmake-build-debug/cmake_install.cmake
+++ lab1/bin2dec/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/supperik/Desktop/главный папочка/OOP labs/lab1/bin2dec
+# Install script for directory: C:/Users/supperik/Desktop/bebra/OOP labs/lab1/bin2dec
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/lab2")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/bin2dec")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/supperik/Desktop/главный папочка/OOP labs/lab1/bin2dec/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/supperik/Desktop/bebra/OOP labs/lab1/bin2dec/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- lab2/vector4/vector4.cpp
+++ lab2/vector4/vector4.cpp
@@ -10,73 +10,67 @@
 // максимального и минимального элементов исходного массива
 
 #include <vector>
-#include <string>
 #include <iostream>
-#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include <limits>
+#include <float.h>
 
-bool isStringCanBeConvertToDouble(const std::string& str)
+const std::string inputValueError = "Value Error: Readed invalid string: ";
+const std::string inputValueLimitsError = "Value Error: Enter smaller number than last. Max length of number: ";
+
+std::vector<float> recordData(std::istream& input)
 {
-    bool result = true;
-    try
-    {
-        double d = stod(str);
-    }
-    catch (const std::invalid_argument& e)
-    {
-        result = false;
-    }
-    catch (const std::out_of_range& e)
-    {
-        result = false;
-    }
-    return result;
-}
+    std::vector<float> vector;
+    std::string stringValue;
+    float value = 0;
 
-std::vector<double> recordData()
-{
-    std::vector<double> inputData;
-
-    std::istringstream iss;
-    std::string inputString;
-    std::string stringStreamWord;
-    double num;
-    bool exitFlag = false;
-
-    while (!exitFlag)
+    while (input >> value)
     {
-        std::cin >> inputString;
-        while(iss >> stringStreamWord)
+        try
         {
-            if (isStringCanBeConvertToDouble(stringStreamWord))
-            {
-                num = stod(stringStreamWord);
-                inputData.push_back(num);
-            }
-            else
-            {
-                std::cout << "Enter string without letter";
-            }
-            if (stringStreamWord == "/")
-            {
-                exitFlag = true;
-            }
+            vector.push_back(value);
+        }
+        catch (std::exception &e)
+        {
+            std::cerr << inputValueError << &e;
         }
     }
-    return inputData;
+
+    return vector;
 }
 
-void handleVector(std::vector<double> &nums)
+void divideVectorElemsOnHalfOfMaxElemInVector(std::vector<float>& nums)
 {
-    for (double num: nums)
+    std::cout << (*std::max_element(nums.begin(), nums.end()) / 2) << '\n';
+    for (float num: nums)
     {
+        num = num / (*std::max_element(nums.begin(), nums.end()) / 2);
+
         std::cout << num << ' ';
+    }
+    std::cout << '\n';
+}
+
+void PrintSortedVector(std::vector<float> vector, std::ostream& output)
+{
+    std::sort(vector.begin(), vector.end());
+    output << std::fixed << std::setprecision(2);
+    for (int i = 0; i < vector.size(); i++)
+    {
+        output << vector[i];
+        if (i != vector.size() - 1)
+        {
+            output << " ";
+        }
     }
 }
 
 int main()
 {
-    std::vector<double> nums = recordData();
-    handleVector(nums);
+    std::vector<float> nums = recordData(std::cin);
+    divideVectorElemsOnHalfOfMaxElemInVector(nums);
+    PrintSortedVector(nums, std::cout);
 
     return 0;
 }
